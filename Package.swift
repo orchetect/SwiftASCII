6c01ae57c9842a169d9e3c4b019615951307535d
Steffan Andrews <stef@orchetect.com>
2021-07-27 23:04:09 -0700

    Added tvOS and watchOS platforms to Package.swift

--- Package.swift
+++ Package.swift
@@ -4,32 +4,28 @@
 import PackageDescription
 
 let package = Package(
-	
+    
     name: "SwiftASCII",
-	
-	platforms: [.macOS(.v10_11), .iOS(.v9)],
-	
+    
+    platforms: [.macOS(.v10_11), .iOS(.v9), .tvOS(.v9), .watchOS(.v2)],
+    
     products: [
-        
         .library(
             name: "SwiftASCII",
             targets: ["SwiftASCII"])
-		
     ],
-	
+    
     dependencies: [
         // none
     ],
-	
+    
     targets: [
-        
         .target(
             name: "SwiftASCII",
             dependencies: []),
         .testTarget(
             name: "SwiftASCIITests",
             dependencies: ["SwiftASCII"])
-		
     ]
-	
+    
 )
