da8f911cf08766dfe2693926b01ef0cbd104dc71
Steffan Andrews <stef@orchetect.com>
2024-09-23 14:24:06 -0700

    Removed stale unit test file

--- Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-//
-//  LinuxMain.swift
-//  SwiftASCII • https://github.com/orchetect/SwiftASCII
-//  © 2022 Steffan Andrews • Licensed under MIT License
-//
-
-fatalError("Run the tests with `swift test --enable-test-discovery`")
