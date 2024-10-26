91f4cc6b4125c15146facb12e7cd5bb6c1e9760b
Steffan Andrews <stef@orchetect.com>
2024-10-26 00:57:16 -0700

    Package.swift cleanup

--- Package.swift
+++ Package.swift
@@ -1,24 +1,17 @@
 // swift-tools-version:5.3
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// (be sure to update the .swift-version file when this Swift version changes)
 
 import PackageDescription
 
 let package = Package(
     name: "SwiftASCII",
-    
     platforms: [.macOS(.v10_11), .iOS(.v9), .tvOS(.v9), .watchOS(.v2)],
-    
     products: [
         .library(
             name: "SwiftASCII",
             targets: ["SwiftASCII"]
         )
     ],
-    
-    dependencies: [
-        // none
-    ],
-    
     targets: [
         .target(
             name: "SwiftASCII",
